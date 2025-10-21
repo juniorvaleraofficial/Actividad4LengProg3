c50db0909fb80666f6b4a01532e05873badd1027
Junior Valera <detoprox@gmail.com>
2025-10-21 18:58:55 -0400

    Creacion de modelos Carrera y Recinto

---
+++ Actividad3LengProg3/Models/CarreraViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class CarreraViewModel
+    {
+        [Required]
+        public int Codigo { get; set; }
+
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        public int CantidadCreditos { get; set; }
+
+        [Required]
+        public int CantidadMaterias { get; set; }
+    }
+}
---
+++ Actividad3LengProg3/Models/RecintoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class RecintoViewModel
+    {
+        [Required]
+        public int Codigo { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Direccion { get; set; } = string.Empty;
+    }
+}
