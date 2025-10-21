167f82dca4fe86781b15fb1be2864cd89bce4ce7
Junior Valera <detoprox@gmail.com>
2025-10-21 19:05:35 -0400

    Controladores Carrera y Recinto

---
+++ Actividad3LengProg3/Controllers/CarrerasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Actividad4LengProg3.Models;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class CarrerasController : Controller
+    {
+        private static List<CarreraViewModel> listaCarreras = new();
+
+        public IActionResult Index()
+        {
+            return View(listaCarreras);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CarreraViewModel carrera)
+        {
+            if (ModelState.IsValid)
+            {
+                listaCarreras.Add(carrera);
+                return RedirectToAction("Index");
+            }
+            return View(carrera);
+        }
+
+        public IActionResult Delete(int codigo)
+        {
+            var c = listaCarreras.FirstOrDefault(x => x.Codigo == codigo);
+            if (c != null) listaCarreras.Remove(c);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Actividad3LengProg3/Controllers/RecintosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Actividad4LengProg3.Models;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class RecintosController : Controller
+    {
+        private static List<RecintoViewModel> listaRecintos = new();
+
+        public IActionResult Index()
+        {
+            return View(listaRecintos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(RecintoViewModel recinto)
+        {
+            if (ModelState.IsValid)
+            {
+                listaRecintos.Add(recinto);
+                return RedirectToAction("Index");
+            }
+            return View(recinto);
+        }
+
+        public IActionResult Delete(int codigo)
+        {
+            var r = listaRecintos.FirstOrDefault(x => x.Codigo == codigo);
+            if (r != null) listaRecintos.Remove(r);
+            return RedirectToAction("Index");
+        }
+    }
+}
